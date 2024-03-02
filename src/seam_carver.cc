48af538bc9503c8a25afb82d25424fc618cf8ee9
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-02 17:18:41 -0600

    modified GetEnergy implementation to handle wrap around cases

--- src/seam_carver.cc
+++ src/seam_carver.cc
@@ -32,16 +32,38 @@ int SeamCarver::GetWidth() const { return width_; }
 // may assume that row and col will always be within bounds.
 int SeamCarver::GetEnergy(int row, int col) const {
   // calculating column energy (horizontal)
-  Pixel left = image_.GetPixel(row, (col - 1));
-  Pixel right = image_.GetPixel(row, (col + 1));
-  int r_col = left.GetRed() + right.GetRed();
-  int g_col = left.GetGreen() - right.GetGreen();
-  int b_col = left.GetBlue() - right.GetBlue();
+  Pixel left;
+  // handling wrap around cases
+  if (col - 1 < 0) {
+    left = image_.GetPixel(row, width_ - 1);
+  } else {
+    left = image_.GetPixel(row, col - 1);
+  }
+  Pixel right;
+  if (col + 1 > width_ - 1) {
+    right = image_.GetPixel(row, 0);
+  } else {
+    right = image_.GetPixel(row, (col + 1));
+  }
+  int r_col = right.GetRed() - left.GetRed();
+  int g_col = right.GetGreen() - left.GetGreen();
+  int b_col = right.GetBlue() - left.GetBlue();
   int col_change_squared = r_col * r_col + g_col * g_col + b_col * b_col;
 
   // calculating row energy (vertical)
-  Pixel top = image_.GetPixel((row - 1), col);
-  Pixel bottom = image_.GetPixel((row + 1), col);
+  // handing wrap around cases
+  Pixel top;
+  if (row - 1 < 0) {
+    top = image_.GetPixel((height_ - 1), col);
+  } else {
+    top = image_.GetPixel((row - 1), col);
+  }
+  Pixel bottom;
+  if (row + 1 > height_ - 1) {
+    bottom = image_.GetPixel(0, col);
+  } else {
+    bottom = image_.GetPixel((row + 1), col);
+  }
   int r_row = top.GetRed() - bottom.GetRed();
   int g_row = top.GetGreen() - bottom.GetGreen();
   int b_row = top.GetBlue() - bottom.GetBlue();
