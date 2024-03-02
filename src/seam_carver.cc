d2cd487505446817e80a5c7e0886ebc9282f5e18
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-02 17:04:02 -0600

    implemented energy calculation algorithm

--- src/seam_carver.cc
+++ src/seam_carver.cc
@@ -30,7 +30,27 @@ int SeamCarver::GetWidth() const { return width_; }
 
 // Returns the energy of the pixel at (row, col) of the instance’s image. You
 // may assume that row and col will always be within bounds.
-int SeamCarver::GetEnergy(int row, int col) const {}
+int SeamCarver::GetEnergy(int row, int col) const {
+  // calculating column energy (horizontal)
+  Pixel left = image_.GetPixel(row, (col - 1));
+  Pixel right = image_.GetPixel(row, (col + 1));
+  int r_col = left.GetRed() + right.GetRed();
+  int g_col = left.GetGreen() - right.GetGreen();
+  int b_col = left.GetBlue() - right.GetBlue();
+  int col_change_squared = r_col * r_col + g_col * g_col + b_col * b_col;
+
+  // calculating row energy (vertical)
+  Pixel top = image_.GetPixel((row - 1), col);
+  Pixel bottom = image_.GetPixel((row + 1), col);
+  int r_row = top.GetRed() - bottom.GetRed();
+  int g_row = top.GetGreen() - bottom.GetGreen();
+  int b_row = top.GetBlue() - bottom.GetBlue();
+  int row_change_squared = r_row * r_row + g_row * g_row + b_row * b_row;
+
+  // calculating energy E
+  int energy = row_change_squared + col_change_squared;
+  return energy;
+}
 
 // Returns the horizontal seam of image_ with the least amount of energy. Notice
 // the array returned by this method is on the free store; keep in mind when the
